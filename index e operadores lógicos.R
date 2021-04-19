6524615cb31ce71259bc0ee05fc832253faada3d
Lucas Feitosa <lucaas.feitosa@hotmail.com>
2021-04-19 00:48:44 -0300

    index e operadores lógicos

---
+++ index e operadores lógicos.R
@@ -0,0 +1,8 @@
+#vetor emissões de co2 a partir de combustíveis fósseis
+co2_emissions <- c(93.92, 104.83, 325.33, 781.58, 326.05, 181.91, 313.77, 51.78,  74.80, 260.95, 38.27, 358.13, 35.7, 402.55, 233.47)
+
+#acessando o quarto elemento
+co2_emissions[4]
+
+#acessando os elementos 3, 7 até 11 e 14
+co2_emissions[c(3, 7:11, 14)]
